c19b69a1190141b81225bc780502ad57c73e5b36
peammapake <34181322+peammapake@users.noreply.github.com>
2017-12-07 17:31:41 +0700

    Add files via upload

---
+++ MAIN/search.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int titleSearch(MOVIE* movie, int* header, char search)
+int actorSearch(MOVIE* movie, int* header, char search)
+int categorySearch(MOVIE* movie, int* header, char search)
+int releaseSearch(MOVIE* movie, int* header, char search)
+int displaySearch(int count, int* position)
+void searchData(MOVIE_T* movie, int* header, int choice)
+int searchOption(MOVIE_T* movie, int *header)
