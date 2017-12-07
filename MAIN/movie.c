0f6603601db6998315ba9786c3b702c0dbfbe557
peammapake <34181322+peammapake@users.noreply.github.com>
2017-12-07 16:49:25 +0700

    Add files via upload

--- MAIN/movie.c
+++ MAIN/movie.c
@@ -2,9 +2,9 @@
 *		Program name : movie.c			
 *		Group name : Apple Juice
 *			Member : 1. 
-*					 2.
-*					 3.
-*					 4.
+*				 2.
+*				 3.
+*				 4.
 *			
 *			6 December 2017
 *
@@ -16,11 +16,36 @@
 #include<ctype.h>
 #include<time.h>
 
+#include "movie.h"
+#include "../displayData/displayData.h"
+#include "../addData/addData.h"
+#include "../ioFunction/ioFunction.h"
+
 int main()
 {
+	FILE* pIn = NULL;
+	MOVIE_T* movie;
 	char terminalInput[256];
 	int choice = 0;
-	
+	int header = 0;
+
+	pIn = fopen("../DATABASE.txt", "r");
+	if(pIn == NULL)
+	{
+		printf("DATABASE.txt file not found\n");
+		printf("Creating new file...\n");
+		pIn = fopen("../DATABASE.txt", "w");
+		if(pIn == NULL)
+		{
+			printf("Error - Cannot create file.\n");
+			exit(1);
+		}
+		printf("Create file success\n\n");
+	}
+	fclose(pIn);
+	readHeader(header);
+	movie = (MOVIE_T*) calloc(header, sizeof(MOVIE_T));
+	readData(movie);
 	printf("===================================");
 	printf("\nWELCOME TO YOUR MOVIE DATABASE\n");
 	printf("===================================\n\n");
@@ -39,12 +64,15 @@ int main()
 		{
 			case 1 :
 				printf("Display!\n");
+				displayData(movie, header);
 				break;
 			case 2 :
 				printf("Add!\n");
+				addData(movie, header);
 				break;
 			case 3 :
 				printf("Edit!\n");
+				
 				break;
 			case 4 :
 				printf("Delete!\n");
@@ -54,10 +82,12 @@ int main()
 				break;
 			case 6 :
 				printf("EXIT! Goodbye!\n");
+				fclose(pIn);
 				exit(1);
 			default :
 				printf("Error - Command not found");
 		}
+	fclose(pIn);
 	}
 	return 0;
 }
